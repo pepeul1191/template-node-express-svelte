deadb243d3daff5e3b192941919d6856cc90d0c6
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2026-02-15 16:29:19 -0500

    dashboard en ejs con rollup

--- src/entries/web.js
+++ src/entries/web.js
@@ -1,3 +1,27 @@
 // src/entries/app.js
 import '../stylesheets/styles.css';
 import '../stylesheets/web.css';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggleSidebar");
+    
+    // 1️⃣ Aplicar estado guardado al cargar
+    const sidebarState = localStorage.getItem("sidebarState");
+    if (sidebarState === "collapsed") {
+        sidebar.classList.add("collapsed");
+    }
+
+    console.log(toggleBtn)
+
+    // 2️⃣ Evento toggle + guardar estado
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapsed");
+
+        if (sidebar.classList.contains("collapsed")) {
+            localStorage.setItem("sidebarState", "collapsed");
+        } else {
+            localStorage.setItem("sidebarState", "expanded");
+        }
+    });
+});
